3e4e1bb9475b2004ca24a1cd929a8e31e8036e5d
Mauricio Muratori <maurimura@gmail.com>
2021-08-25 20:48:46 -0300

    Call and ret methods added

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use std::panic;
+
 struct CPU {
     position_in_memory: usize,
     registers: [u8; 16],
     memory: [u8; 0x1000],
     stack: [u16; 16],
-    stack_pointer: usize
+    stack_pointer: usize,
 }
 
 impl CPU {
@@ -25,14 +27,41 @@ impl CPU {
             let y = ((opcode & 0x00F0) >> 4) as u8;
             let d = ((opcode & 0x000F) >> 0) as u8;
 
+            let nnn = opcode & 0x0FFF;
+
             match (c, x, y, d) {
                 (0, 0, 0, 0) => {
                     return;
                 }
+                (0, 0, 0xE, 0xE) => self.ret(),
+                (0x2, _, _, _) => self.call(nnn),
                 (0x8, _, _, 0x4) => self.add_xy(x, y),
                 _ => todo!("opcode {:04x}", opcode),
             }
         }
+    }
+
+    fn call(&mut self, addr: u16) {
+        let sp = self.stack_pointer;
+        let stack = &mut self.stack;
+
+        if sp > stack.len() {
+            panic!("Stack overflow")
+        }
+
+        stack[sp] = self.position_in_memory as u16;
+        self.stack_pointer += 1;
+        self.position_in_memory = addr as usize;
+    }
+
+    fn ret(&mut self) {
+        if self.stack_pointer == 0 {
+            panic!("Stack underflow")
+        }
+
+        self.stack_pointer -= 1;
+        let call_addr = self.stack[self.stack_pointer];
+        self.position_in_memory = call_addr as usize;
     }
 
     fn add_xy(&mut self, x: u8, y: u8) {
@@ -57,22 +86,23 @@ fn main() {
         registers: [0; 16],
         memory: [0; 4096],
         stack: [0; 16],
-        stack_pointer: 0
+        stack_pointer: 0,
     };
+
+    let mem = &mut cpu.memory;
 
     cpu.registers[0] = 5;
     cpu.registers[1] = 10;
-    cpu.registers[2] = 10;
-    cpu.registers[3] = 10;
 
-    let mem = &mut cpu.memory;
-    mem[0] = 0x80; mem[1] = 0x14;
-    mem[2] = 0x80; mem[3] = 0x24;
-    mem[4] = 0x80; mem[5] = 0x34;
-    
+    let caller = [0x21, 0x00, 0x21, 0x00, 0x00, 0x00];
+
+    let add_twice = [0x80, 0x14, 0x80, 0x14, 0x00, 0xEE];
+    mem[0x000..0x006].copy_from_slice(&caller);
+    mem[0x100..0x106].copy_from_slice(&add_twice);
+
     cpu.run();
 
-    assert_eq!(cpu.registers[0], 35);
+    assert_eq!(cpu.registers[0], 45);
 
     println!("5 + 10 + 10 + 10 = {}", cpu.registers[0]);
 }
